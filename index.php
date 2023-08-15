828c7e0e4c5677aa3eb56e415c44941c800f35b4
Marta Domagała <mahomegol@gmail.com>
2023-08-15 12:21:31 +0200

    Separate html from php

--- index.php
+++ index.php
@@ -9,42 +9,14 @@ require_once("src/Debug/debug.php");
 //Jeśli istnieje ta zmienna to weź wartość która jest pod kluczem i przypisz jeśli nie to NULL
 $action = $_GET['action'] ?? null;
 
-?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Moje notatki</title>
-</head>
-<body>
-    <div class="header">
-        <h1>Moje notatki</h1>
-    </div>
-        <div class="container">
-            <div class="menu">
-            <ul>
-                <li>
-                    <a href="./">Lista notatek</a>
-                </li>
-                <li>
-                    <a href="./?action=create">Nowa notatka</a>
-                </li>
-            </ul>
-        </div>
-        <div>
-            <?php if($action === 'create'): ?>
-                    <h3>Nowa notatka</h3>
-                <?php echo htmlentities($action) ?>
-            <?php else: ?>
-                    <h4>Lista notatek</h4>
-                <?php htmlentities($action ?? '') ?>
-                <?php endif;?>
-            
-        </div>
-    </div>
-    <div class="footer">
 
-    </div>
-</body>
-</html>
+if($action ==='list')
+{
+    include_once("templates/Pages/create.php");
+}
+else
+{
+    include_once("templates/Pages/list.php");
+}
+
+
---
+++ templates/Pages/create.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Moje notatki</title>
+</head>
+<body>
+    <div class="header">
+        <h1>Moje notatki</h1>
+    </div>
+        <div class="container">
+            <div class="menu">
+            <ul>
+                <li>
+                    <a href=".\">Lista notatek</a>
+                </li>
+                <li>
+                    <a href=".\?action=create">Nowa notatka</a>
+                </li>
+            </ul>
+        </div>
+        <div>
+                 <h3>Nowa notatka</h3>
+                <?php echo htmlentities($action);
+                    var_dump($action);
+                ?>
+        </div>
+    </div>
+
+    <div class="footer">
+    </div>
+</body>
+</html>
