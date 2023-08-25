d9d86890670a4390e518b30f0b40dcef376533e3
Marta Domagała <mahomegol@gmail.com>
2023-08-25 13:45:41 +0200

    Create implements of service database

--- src/Database.php
+++ src/Database.php
@@ -14,22 +14,51 @@ use Throwable;
 
 class Database 
 {
+    private PDO $conn;
 
     public function __construct(array $config)
     {
         try {
-                $this->validateConfig($config);
-            
-                $dsn = "mysql:dbname={$config['database']};host={$config['host']}";
-                $connection = new PDO(
-                $dsn,
-                $config['user'],
-                $config['password']
-                );
-                dump($connection);
+            $this->validateConfig($config);
+            $this->databaseConnection($config);
         }catch (PDOException $e){
             throw new StorageException('Connection error');
         }
+    }
+
+    public function createNote(array $data): void
+    {
+        try {
+            echo 'Tworzymy notatke';
+            dump($data);
+            
+            $title = $this->conn->quote($data['title']);
+            $decsrpition = $this->conn->quote($data['description']);
+            $created = $this->conn->quote(date('Y-m-d H:i:s'));
+
+            $query = "INSERT INTO notes (title, description, created) VALUES($title, $decsrpition, $created)";
+
+            $result = $this->conn->exec($query);
+            dump($result);
+            
+        }catch (Throwable $e){
+            throw new StorageException('Nie udało się dodać nowej notatki', 400, $e);
+            dump($e); exit;
+        }
+    }
+    
+    
+    private function databaseConnection(array $config): void
+    {
+        $dsn = "mysql:dbname={$config['database']};host={$config['host']}";
+                $this->conn = new PDO(
+                $dsn,
+                $config['user'],
+                $config['password'],
+                [
+                    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+                ]
+                );
     }
 
     private function validateConfig(array $config): void
